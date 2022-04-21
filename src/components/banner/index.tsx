7cfaaaa29d499f925e7f8db13dc92b04ebcc4f63
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-04-20 21:46:43 -0300

    Add files via upload

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// CSS-in-JS
+
+const Banner = () => {
+  return (
+    <div>
+      <h1>Rick and Morty</h1>
+      <p>
+        Rick and Morty (em português Rick e Morty) é uma série de animação
+        adulta norte-americana de comédia e ficção científica criada por Justin
+        Roiland e Dan Harmon para o bloco de programação noturno Adult Swim,
+        exibido no canal Cartoon Network.
+      </p>
+      <button>Assistir Trailer</button>
+      <button>Salvar no favoritos</button>
+    </div>
+  );
+};
+
+export default Banner;
